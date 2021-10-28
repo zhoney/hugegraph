28d11d5bf2c07a233b009b9ec4d10e2fe3846c48
guoygang <hero.gariker@gmail.com>
2021-10-28 20:47:52 +0800

    merge gh-pre-release to gh-dis-release (#48)
    * disable etcd for auth server (#42)
    
    * refine MetricsAPI test for prometheus exporter (#43)
    
    * upgrade hugegraph config and white api list (#44)
    
    * fixed computer-dis api bug (#45)
    
    * fix rest-server.properties config item k8s.hugegraph_url (#46)
    
    * fix rocksdb not close after close graph (#47)

--- hugegraph-api/src/main/java/com/baidu/hugegraph/core/GraphManager.java
+++ hugegraph-api/src/main/java/com/baidu/hugegraph/core/GraphManager.java
@@ -138,7 +138,9 @@ public final class GraphManager {
         this.serverStarted();
         this.addMetrics(conf);
         // listen meta changes, e.g. watch dynamically graph add/remove
-        this.listenMetaChanges();
+        if (!conf.get(ServerOptions.AUTH_SERVER)) {
+            this.listenMetaChanges();
+        }
     }
 
     public void destroy() {
@@ -157,7 +159,12 @@ public final class GraphManager {
             LOG.debug("RestServer accepts event 'graph.drop'");
             event.checkArgs(String.class);
             String name = (String) event.args()[0];
-            this.graphs.remove(name);
+            HugeGraph graph = (HugeGraph) this.graphs.remove(name);
+            try {
+                graph.close();
+            } catch (Exception e) {
+                LOG.warn("Failed to close graph", e);
+            }
             return null;
         });
     }
